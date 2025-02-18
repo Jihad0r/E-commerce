854575d4e4b519117ee05a2d24628b82b1d6d980
Jihad <ji.0rabiii@gmail.com>
2025-02-18 23:05:18 +0200

    v_1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -8,6 +9,7 @@ import { connectDB } from "./connectDB.js";
 
 dotenv.config()
 const app = express()
+const __dirname = path.resolve();
 const port = process.env.PORT||5000
 
 app.use(express.json())
@@ -18,6 +20,11 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 
 app.listen(port,()=>{
     console.log("Server running")
