450c98557a13e31ca649bdc9a425f528e18d5b36
Jihad <ji.0rabiii@gmail.com>
2025-02-19 00:39:52 +0200

    v_1

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,17 +1,16 @@
-import mongoose from 'mongoose';
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
-};
-
+}
