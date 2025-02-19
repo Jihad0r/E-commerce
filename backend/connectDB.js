cf6f98a07200606b3c64551afd97c05f4a2d69e3
Jihad <ji.0rabiii@gmail.com>
2025-02-19 20:33:21 +0200

    v_1

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,16 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
 import mongoose from "mongoose";
-
 export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-}
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error(`Error connection to mongoDB: ${error.message}`);
+      process.exit(1);
+    }
+};
+  
--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,11 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./auth.route.js"
 import { connectDB } from "./connectDB.js";
 
-
-dotenv.config()
+dotenv.config();
 const app = express()
-const __dirname = path.resolve();
 const port = process.env.PORT||5000
 
+const __dirname = path.resolve();
 app.use(express.json())
 
 app.use(express.urlencoded({extended:true}))
@@ -20,11 +19,11 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 }
 app.listen(port,()=>{
     console.log("Server running")
